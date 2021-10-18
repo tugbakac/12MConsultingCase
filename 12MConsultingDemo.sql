b51d9d34308a887fb4ae83e9b5249cc0af27a27b
tugbakac <tugbakac45@gmail.com>
2021-10-18 19:05:43 +0300

    addedProjectArea

---
+++ 12MConsultingDemo.sql
@@ -0,0 +1,28 @@
+﻿--değişkenlerin tanımlanması
+declare @malkodu int;
+declare @baslangicTarih int;
+declare @bitisTarih int;
+
+--geçici tablo oluşturulması
+CREATE TABLE #STINewTable
+(SiraNo INT IDENTITY(1,1), IslemTur SMALLINT, EvrakNo VARCHAR(16), Tarih INT, GirisMiktar NUMERIC(25,6), CikisMiktar NUMERIC(25,6), StokMiktar NUMERIC(25,6));
+
+--geçici tabloya verilerin eklenmesi
+Insert into #STINewTable(IslemTur,EvrakNo,Tarih,GirisMiktar,CikisMiktar,StokMiktar)
+select * from STI
+
+--sql kapandığında silinecek olsa da geçici tablo silinmek istenirse aşağıdaki kod kullanılacak
+DROP TABLE #STINewTable;
+
+--islemTur alanı için if kullanılması denendi
+if dbo.STI.IslemTur>=0
+begin
+	select "Giriş" as [IslemTur],EvrakNo, Tarih, Miktar as [Giris Miktar], Miktar as [Cikis Miktar] from STI
+end
+
+--procedür ile tablo verileri listelendi
+create proc proc_Listele
+as 
+select IslemTur,EvrakNo, Tarih, Miktar as [Giris Miktar], Miktar as [Cikis Miktar] from STI
+
+proc_Listele
